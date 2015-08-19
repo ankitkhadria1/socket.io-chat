61b35e7b24dd8a91fe88cabaf6d19e59d8f97ec3
demitriy <thereissuchname@yandex.ru>
2015-08-19 09:19:15 +0300

    create new version

--- server.js
+++ server.js
@@ -1,62 +1,8 @@
-(function () {
-	'use strict';
+var Client = require('./lib/client');
 
-	var http        = require('http');
-	var Chat        = require('./index');
-	var statics     = require('node-static');
-	var db          = require('./lib/db');
-	var mongodb     = require('mongodb');
-	var MongoClient = mongodb.MongoClient;
+var c = new Client();
 
-	var file = new statics.Server('./public');
-
-	var server = http.createServer(function (req, res) {
-		req.addListener('end', function () {
-			file.serve(req, res);
-		}).resume();
+c.test()
+	.then(function () {
+		console.log('then');
 	});
-
-	MongoClient.connect('mongodb://127.0.0.1/test', function (error, dbConnect) {
-		server.listen(3000, '127.0.0.1');
-
-		Chat.setConnect(dbConnect);
-
-		var chatClient = new Chat.Client(server);
-
-		chatClient.on('authenticate', function (socket, data, next) {
-			db.getConnect((function (s) {
-				return function (connect) {
-					connect.collection('users').findOne({
-						username: String(data.username),
-						password: String(data.password)
-					}, function (error, result) {
-						if (result) {
-							socket.auth = true;
-							socket.user = result._id;
-
-							next();
-						} else {
-							next(new Error('invalid credentials'));
-						}
-					});
-				};
-			}(socket)));
-		});
-
-		chatClient.on('connection', function () {
-			//console.log(this.io.sockets.sockets.length);
-		});
-
-		chatClient.on('disconnect', function () {
-			//console.log(this.io.sockets.sockets.length);
-		});
-
-		chatClient.on('error', function (socket, event, error) {
-			console.log('client error', event, error);
-			console.log('client error', event, error.stack);
-			//socket.emit('error');
-		});
-	});
-
-
-}());
--- src/client.es6
+++ src/client.es6
@@ -1,3 +1,15 @@
-import Client from './client/client';
+class PPromise extends Promise {
+	constructor (resolve, reject) {
+		super(resolve, reject);
+	}
+}
+
+class Client {
+	test() {
+		return new PPromise((resolve, reject, progress) => {
+			console.log('promise', progress)
+		})
+	}
+}
 
 export default Client;
